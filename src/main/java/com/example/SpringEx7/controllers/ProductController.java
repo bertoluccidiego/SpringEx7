bb7ca5a4a7272f4b4e337cf9ec31e42160f96677
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2024-01-28 15:04:28 -0300

    # Setup to display a table of products and add them via request parameters

--- src/main/java/com/example/SpringEx7/controllers/ProductController.java
+++ src/main/java/com/example/SpringEx7/controllers/ProductController.java
@@ -1,9 +1,10 @@
 package com.example.SpringEx7.controllers;
 
+import com.example.SpringEx7.models.Product;
 import com.example.SpringEx7.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
@@ -14,8 +15,22 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public String viewAll(Model model) {
+        model.addAttribute("products", productService.findAll());
+
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addOne(@RequestParam String name,
+                         @RequestParam double price,
+                         Model model) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        productService.addOne(product);
+
         model.addAttribute("products", productService.findAll());
 
         return "products.html";
